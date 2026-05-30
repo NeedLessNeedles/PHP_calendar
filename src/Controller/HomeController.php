2a42133a3a165a0611f5b489c772cfa4f8b3de38
NeedLessNeedles <miszczykm.prv@gmail.com>
2026-05-30 19:36:44 +0200

    Basic navigation, templates and Event functionality

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Home controller.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Class HomeController.
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * Index action.
+     *
+     * @return Response HTTP response
+     */
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
