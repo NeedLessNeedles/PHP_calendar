c0499ea756f3aa145e7792f458d980f18d99ae7c
NeedLessNeedles <miszczykm.prv@gmail.com>
2026-06-02 02:28:28 +0200

    Changing identifiers for user functionality

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,16 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\ProfileEmailType;
+use App\Form\ChangePasswordType;
 
-#[IsGranted('ROLE_USER, ROLE_ADMIN')]
+//#[IsGranted('ROLE_USER, ROLE_ADMIN')]
 #[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route(name: 'app_profile', methods: ['GET'])]
-    public function index(): Response
+    #[Route(name: 'app_profile', methods: ['GET', 'POST'])]
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response
     {
+        $user = $this->getUser();
+
+        $emailForm = $this->createForm(ProfileEmailType::class, $user);
+        $passwordForm = $this->createForm(ChangePasswordType::class);
+        $emailForm->handleRequest($request);
+        $passwordForm->handleRequest($request);
+
+        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
+            $entityManager->flush();
+        }
+
+        if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
+            $data = $passwordForm->getData();
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $data['newPassword'])
+            );
+            $entityManager->flush();
+        }
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'emailForm' => $emailForm,
+            'passwordForm' => $passwordForm
         ]);
     }
 
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class)
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'New password',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat password',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
