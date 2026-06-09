d623375f010755e741d50cc82d4ff616aedd6cb0
NeedLessNeedles <miszczykm.prv@gmail.com>
2026-06-09 15:22:33 +0200

    Working fixtures loading

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -1,50 +1,14 @@
 <?php
 
-/**
- * Category fixtures.
- */
-
 namespace App\DataFixtures;
 
+use App\Entity\Event;
 use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Generator;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 
-/**
- * Class CategoryFixtures.
- *
- * @psalm-suppress MissingConstructor
- */
-class CategoryFixtures extends AbstractBaseFixtures
+class CategoryFixture
 {
-    /**
-     * Load data.
-     *
-     * @psalm-suppress PossiblyNullPropertyFetch
-     * @psalm-suppress PossiblyNullReference
-     * @psalm-suppress UnusedClosureParam
-     */
-    public function loadData(): void
-    {
-        if (!$this->manager instanceof ObjectManager || !$this->faker instanceof Generator) {
-            return;
-        }
 
-        $this->createMany(20, 'category', function (int $i) {
-            $category = new Category();
-            $category->setTitle($this->faker->unique()->word);
-            $category->setCreatedAt(
-                \DateTimeImmutable::createFromMutable(
-                    $this->faker->dateTimeBetween('-100 days', '-1 days')
-                )
-            );
-            $category->setUpdatedAt(
-                \DateTimeImmutable::createFromMutable(
-                    $this->faker->dateTimeBetween('-100 days', '-1 days')
-                )
-            );
-
-            return $category;
-        });
-    }
 }
+
